038354a4f6452c274e2dc2f43c9a1f785279f336
Emily Byrne <embyrne13@gmail.com>
2022-08-11 15:41:42 -0400

    user auth

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -2,6 +2,6 @@ const Router = require('express').Router()
 const controller = require('../controllers/UserController')
 
 Router.get('/', controller.getUser)
-Router.post('/', controller.createUser)
+Router.post('/', controller.RegisterUser) //controller.createUser)
 
 module.exports = Router
