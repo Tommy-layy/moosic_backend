29d3538b0c59b84b1011263c59664f613df4eff7
Patrick Knight <pfknight2@gmail.com>
2022-08-11 16:52:41 -0400

    canm login a user

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -2,7 +2,7 @@ const Router = require('express').Router()
 const controller = require('../controllers/UserController')
 
 Router.get('/', controller.getUser)
-Router.get('/:user_id', controller.LoginUser)
+Router.post('/', controller.LoginUser)
 Router.post('/', controller.RegisterUser) //controller.createUser)
 
 module.exports = Router
