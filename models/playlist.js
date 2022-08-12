f651e973de705be7e175c423d98d43978b98a5aa
Emily Byrne <embyrne13@gmail.com>
2022-08-12 09:27:39 -0400

    static associate models

--- models/playlist.js
+++ models/playlist.js
@@ -8,7 +8,14 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Playlist.belongsTo(models.User, {
+        foreignKey: 'userId'
+      })
+      Playlist.belongsToMany(models.Song, {
+        as: 'playlist',
+        through: models.Playlist_song,
+        foreignKey: 'playlistId'
+      })
     }
   }
   Playlist.init(
