83d2f191f0a2f2114e8cd8d1d9b4b3ca79b11c9e
Patrick Knight <pfknight2@gmail.com>
2022-08-12 16:14:49 -0400

    added some testing routes for users to playlists

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -1,7 +1,8 @@
 const Router = require('express').Router()
 const controller = require('../controllers/UserController')
 
-Router.get('/', controller.getUser)
+Router.get('/', controller.getAllUser)
+Router.get('/:user_id', controller.getOneUser)
 Router.post('/', controller.LoginUser)
 Router.post('/', controller.RegisterUser) //controller.createUser)
 
