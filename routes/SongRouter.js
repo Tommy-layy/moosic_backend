796d28a204b90a495eddd4587ee2f944c4a999f9
Emily Byrne <embyrne13@gmail.com>
2022-08-18 10:10:41 -0400

    migrations to fix song model

--- routes/SongRouter.js
+++ routes/SongRouter.js
@@ -3,5 +3,6 @@ const controller = require('../controllers/SongController')
 
 Router.get('/', controller.getFilteredSongs)
 Router.get('/:song_id', controller.getOneSong)
+Router.post('/', controller.addSong)
 
 module.exports = Router
